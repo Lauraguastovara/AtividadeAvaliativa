6f8df44dbf94bbf665b3684a77d228b1ede70d52
Lauraguastovara <104375298+Lauraguastovara@users.noreply.github.com>
2023-06-15 16:46:55 -0400

    Delete blog-laura directory

--- blog-laura/app/Libraries/Database.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $dbh = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
-?>
